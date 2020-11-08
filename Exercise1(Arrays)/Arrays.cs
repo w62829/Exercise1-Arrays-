f582e6d4f16db9f19fb04ba8eaedb7a516b1c334
w62829 <w62829@student.wsiz.edu.pl>
2020-11-08 20:46:58 +0600

    Add project files.

---
+++ Exercise1(Arrays)/Arrays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise1_Arrays_
+{
+   public static class Arrays
+    {
+        public static int[] Accumulate(int[] inputArray)
+        {
+            var result = new int[inputArray.Length];
+            for(var i = 0; i < inputArray.Length; i++)
+            {
+                var j = i + 1;
+                var k = i;
+                while(k<j)
+                {
+                    result[i] += inputArray[k];
+                    k++;
+                    
+                }
+               
+            }
+            return result; 
+        }
+
+    }
+}
---
+++ Exercise1(Arrays)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise1_Arrays_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            var result = Arrays.Accumulate(arr);
+            foreach(var element in result)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
